3d6b50c3161bc66281e83899ea1b210cb32a423d
VaniushaR <vaniusha.ra@gmail.com>
2018-08-04 19:04:09 -0500

    Creando Rama SplashView

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+//view1 loadig charge
+window.onload {
+    const load = document.getElementById('load');
+    load.addEventListener('click', loading);
+    const loading = () => {
+        
+    }
+}
