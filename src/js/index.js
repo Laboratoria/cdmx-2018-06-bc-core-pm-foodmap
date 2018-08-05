d14dce9d39980f209ebfc32d1ff8b3da370eb6cc
VaniushaR <vaniusha.ra@gmail.com>
2018-08-05 13:18:41 -0500

    Agregando barra de navegación responsive

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,15 @@
 //configurar función timer para que la vista Splash sólo dure unos segundos
-const splash = document.getElementById('splash');
-window.onload.initialization();
-const initialization = () => {
-  splash.style.display = 'none';
-};
+// const splash = document.getElementById('splash');
+// window.onload.initialization();
+// const initialization = () => {
+//   splash.style.display = 'none';
+// };
 
+//inicializador para responsive NavBar
+
+document.addEventListener('DOMContentLoaded', function() {
+  let elems = document.querySelectorAll('.sidenav');
+  let instances = M.Sidenav.init(elems);
+  console.log(instances);
+});
 //API GOOGLE MAPS
