c42611eeda9f5c566132f30bf20a45f4195d61ad
Palmidia Jetzabel Garay Dávila <gjetzable@gmail.com>
2018-08-05 15:10:46 -0500

    Subiendo cambios en estilo de loader, agregando cambios para obtener ubicación del usuario

--- src/js/mainPage.js
+++ src/js/mainPage.js
@@ -1,9 +1,41 @@
 window.initMap = () => {
-  let cdmx = { lat: 19.364456, 
-    lng: -99.187620};
+  let position = currentPosition();
   let map = new google.maps.Map(
-    document.getElementById('map'), { zoom: 4, 
-      center: cdmx });
-  let marker = new google.maps.Marker({ position: cdmx, 
-    map: map });
+    document.getElementById('map'), {
+      zoom: 13,
+      center: position
+    });
+  let marker = new google.maps.Marker({
+    position: position,
+    map: map
+  });
 };
+
+window.currentPosition = () => {
+  // Get user's current position.
+  let options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  };
+
+  const success = (pos) => {
+    let crd = pos.coords;
+
+    console.log('Your current position is:');
+    console.log('Latitude : ' + crd.latitude);
+    console.log('Longitude: ' + crd.longitude);
+    console.log('More or less ' + crd.accuracy + ' meters.');
+  };
+
+  const error = (err) => {
+    console.warn('ERROR(' + err.code + '): ' + err.message);
+  };
+
+  navigator.geolocation.getCurrentPosition(success, error, options);
+};
+
+document.getElementById('search-btn').addEventListener('click', event => {
+  let placeToSearch = document.getElementById('search-term').value;
+  console.log(placeToSearch);
+});
