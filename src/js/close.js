79e116450ec2927456fd932834cf61ea344bdf16
DianaCarrillo <39560661+DianaCarrillo@users.noreply.github.com>
2018-08-05 09:16:19 -0500

    subiendo cambios de mapa méxico

--- src/js/close.js
+++ src/js/close.js
@@ -3,5 +3,6 @@ setTimeout(()=>{
     // quiero que cuando la animación se acabe, se agregue una clase
     container.classList.add('close');
     // después de 2 segundos, se le agregue la clase cerrar
-    location.href = '../src/index.html';
+    location.href = 'views/view1.html';
  },2000); 
+ 
