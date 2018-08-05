ed75bbc9b15e07af3f38a232b6b8c08e38bf0b1d
Palmidia Jetzabel Garay Dávila <gjetzable@gmail.com>
2018-08-04 20:25:42 -0500

    Subiendo función para agregar mapa

---
+++ src/js/mainPage.js
@@ -0,0 +1,9 @@
+const initMap = () => {
+  let cdmx = { lat: 19.364456, 
+    lng: -99.187620};
+  let map = new google.maps.Map(
+    document.getElementById('map'), { zoom: 4, 
+      center: cdmx });
+  let marker = new google.maps.Marker({ position: cdmx, 
+    map: map });
+};
