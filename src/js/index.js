bef33cbe463706affb575d0e73383ca3819577b7
RaquelGlez <gonzalez.c.raquel@gmail.com>
2018-08-06 11:00:05 -0500

    Se agrega pagina de inicio

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,7 @@
-alert('hola')
+
+window.onload = () => {
+  const contenedor = document.getElementById('container-carga');
+  contenedor.style.visibility = 'hidden';
+  contenedor.style.opacity = '0';
+  location.href = '../src/views/view1.html';
+};
