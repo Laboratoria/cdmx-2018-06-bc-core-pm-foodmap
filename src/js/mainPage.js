a6e7fc5aaa539149d514715cdd3544c2f6c4b2dd
Palmidia Jetzabel Garay Dávila <gjetzable@gmail.com>
2018-08-05 20:38:52 -0500

    Subiendo cambios a funcion inicial

--- src/js/mainPage.js
+++ src/js/mainPage.js
@@ -1,18 +1,20 @@
-window.initMap = () => {
-  let position = currentPosition();
-  let map = new google.maps.Map(
-    document.getElementById('map'), {
-      zoom: 13,
-      center: position
-    });
-  let marker = new google.maps.Marker({
-    position: position,
-    map: map
-  });
-};
+// window.initMap = (lat, long) => {
+//   currentPosition();
+//   let initialPosition = {lat: lat, 
+//     lng: long};
+//   let map = new google.maps.Map(
+//     document.getElementById('map'), {
+//       zoom: 9,
+//       center: initialPosition
+//     });
+//   let marker = new google.maps.Marker({
+//     position: initialPosition,
+//     map: map
+//   });
+// };
 
 window.currentPosition = () => {
-  // Get user's current position.
+  // Get user's current/initial position.
   let options = {
     enableHighAccuracy: true,
     timeout: 5000,
@@ -21,11 +23,21 @@ window.currentPosition = () => {
 
   const success = (pos) => {
     let crd = pos.coords;
-
     console.log('Your current position is:');
     console.log('Latitude : ' + crd.latitude);
     console.log('Longitude: ' + crd.longitude);
     console.log('More or less ' + crd.accuracy + ' meters.');
+    let initialPosition = {lat: crd.latitude, 
+      lng: crd.longitude};
+    let map = new google.maps.Map(
+      document.getElementById('map'), {
+        zoom: 9,
+        center: initialPosition
+      });
+    let marker = new google.maps.Marker({
+      position: initialPosition,
+      map: map
+    });
   };
 
   const error = (err) => {
