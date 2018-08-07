ea55c3679e41b0083f9d36db1cdfa4fa6b628918
Palmidia Jetzabel Garay Dávila <gjetzable@gmail.com>
2018-08-07 07:20:03 -0500

    Subiendo cambios en función para busqueda

--- src/js/mainPage.js
+++ src/js/mainPage.js
@@ -1,53 +1,10 @@
-// window.initMap = (lat, long) => {
-//   currentPosition();
-//   let initialPosition = {lat: lat, 
-//     lng: long};
-//   let map = new google.maps.Map(
-//     document.getElementById('map'), {
-//       zoom: 9,
-//       center: initialPosition
-//     });
-//   let marker = new google.maps.Marker({
-//     position: initialPosition,
-//     map: map
-//   });
-// };
+initializeFirebase();
+let db = firebase.firestore();
+let dbSettings = { timestampsInSnapshots: true };
+db.settings(dbSettings);
 
-window.currentPosition = () => {
-  // Get user's current/initial position.
-  let options = {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0
-  };
-
-  const success = (pos) => {
-    let crd = pos.coords;
-    console.log('Your current position is:');
-    console.log('Latitude : ' + crd.latitude);
-    console.log('Longitude: ' + crd.longitude);
-    console.log('More or less ' + crd.accuracy + ' meters.');
-    let initialPosition = {lat: crd.latitude, 
-      lng: crd.longitude};
-    let map = new google.maps.Map(
-      document.getElementById('map'), {
-        zoom: 9,
-        center: initialPosition
-      });
-    let marker = new google.maps.Marker({
-      position: initialPosition,
-      map: map
-    });
-  };
-
-  const error = (err) => {
-    console.warn('ERROR(' + err.code + '): ' + err.message);
-  };
-
-  navigator.geolocation.getCurrentPosition(success, error, options);
-};
 
 document.getElementById('search-btn').addEventListener('click', event => {
   let placeToSearch = document.getElementById('search-term').value;
-  console.log(placeToSearch);
+  let searchByInput = document.getElementById('search-term').value;
 });
