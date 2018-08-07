76fafe58ca866899ce00bd9e10108ef10644c797
RaquelGlez <gonzalez.c.raquel@gmail.com>
2018-08-07 13:06:18 -0500

    Agrega avance en funcion de busqueda y readme

--- src/js/index.js
+++ src/js/index.js
@@ -5,3 +5,27 @@ window.onload = () => {
   contenedor.style.opacity = '0';
   location.href = '../src/views/view1.html';
 };
+
+
+// Guardamos la ubicación de la data en una variable
+const json = 'https://raw.githubusercontent.com/RaquelGlez/cdmx-2018-06-bc-core-pm-foodmap/master/data/data.json';
+window.onload = () => {
+  fetch(json)
+    .then(response => response.json())
+    .then((data) =>{
+      search(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+const searchInput = document.getElementById('search');
+const btn = document.getElementById('btn-search');
+
+const search = (data) => {
+  textSearch = searchInput.value;
+  console.log(textSearch);
+};
+
+btn.addEventListener('click', search);
