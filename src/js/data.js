d5cbfcb5b02c07c648f38bafaf55901148b58c83
VaniushaR <vaniusha.ra@gmail.com>
2018-08-07 09:19:14 -0500

    Agregando mapa con geolocalización

--- src/js/data.js
+++ src/js/data.js
@@ -25,15 +25,60 @@
 // console.log(userLocation);
 alert('conectado el data.js');
 
-const initialize = () => {
-  let center = new google.maps.LatLng(37.442, -122, 084058);
-  console.log(center);
-  let map = new google.maps.Map(document.getElementById('map'), {
-    center: center,
-    zoom: 13
-  });
-  console.log(map);
-};
+// const initialize = () => {
+//   let center = new google.maps.LatLng(37.442, -122, 084058);
+//   console.log(center);
 
-window.onload = initialize;
+//   let map = new google.maps.Map(document.getElementById('map'), {
+//     center: center,
+//     zoom: 13
+//   });
+//   console.log(map);
+// };
+
+// window.onload = initialize;
 // google.maps.event.addDomListener(window, 'load', initialize);
+
+var map, infoWindow;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 6
+  });
+  infoWindow = new google.maps.InfoWindow();
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      function(position) {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+
+        infoWindow.setPosition(pos);
+        infoWindow.setContent('Location found.');
+        infoWindow.open(map);
+        map.setCenter(pos);
+      },
+      function() {
+        handleLocationError(true, infoWindow, map.getCenter());
+      }
+    );
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? 'Error: The Geolocation service failed.'
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
+}
+window.onload = initMap;
